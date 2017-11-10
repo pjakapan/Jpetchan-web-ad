ce81f8f64d672b468d12f4453f8ffb47ab8d9448
pjakapan <j.petchan@togetherteam.co.th>
2017-11-10 13:25:40 +0700

    Create sample web-ad

---
+++ TodoSPA/App/Scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+angular.module('todoApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/TodoList", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+        requireADLogin: true,
+    }).otherwise({ redirectTo: "/Home" });
+
+    adalProvider.init(
+        {
+            instance: 'https://login.microsoftonline.com/', 
+            tenant: 'common',
+            clientId: '5a6276f1-7d38-45fe-bf4f-3db5fe3eb7c7',
+            popUp: false,
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        },
+        $httpProvider
+        );
+   
+}]);
